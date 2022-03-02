9dde015f096715f66f71d76aabd28c1d77fff915
Nikita Lezhnev <catassasin331@gmail.com>
2022-03-02 10:51:25 +0300

    Add route for making campgrounds

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const path = require('path');
+const Campground = require('./models/campground');
+
+mongoose.connect('mongodb://localhost:27017/yelpcamp');
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error'));
+db.once('open', () => {
+  console.log('Database connected');
+});
 
 const app = express();
 
@@ -8,6 +18,15 @@ app.set('views', path.join(__dirname, 'views'));
 
 app.get('/', (req, res) => {
   res.render('home');
+});
+
+app.get('/makecampground', async (req, res) => {
+  const camp = new Campground({
+    title: 'My Backyard',
+    description: 'Dirt cheap camping',
+  });
+  await camp.save();
+  res.send(camp);
 });
 
 app.listen(3000, () => {

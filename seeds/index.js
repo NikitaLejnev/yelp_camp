827607e78dbf9bb90da1577b221226a756a5ab7b
Nikita Lezhnev <catassasin331@gmail.com>
2022-03-02 11:27:33 +0300

    Change seedDb to create 50 campground with random location

--- seeds/index.js
+++ seeds/index.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const Campground = require('../models/campground');
+const cities = require('./cities');
 
 mongoose.connect('mongodb://localhost:27017/yelpcamp');
 
@@ -11,8 +12,17 @@ db.once('open', () => {
 
 const seedDb = async () => {
   await Campground.deleteMany({});
-  const c = new Campground({ title: 'Drowsy Hills' });
-  await c.save();
+  const camps = [];
+  for (let i = 0; i < 50; i += 1) {
+    const randomIndex = Math.floor(Math.random() * 1000);
+    const { city, state } = cities[randomIndex];
+    const camp = new Campground({
+      location: `${city}, ${state}`,
+    });
+    camps.push(camp);
+  }
+  const results = await Promise.all(camps);
+  await Campground.create(results);
 };
 
 seedDb();

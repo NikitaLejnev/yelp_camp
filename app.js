657cbf0c1785c47f0bb7ca2eced302bf4497f741
Nikita Lezhnev <catassasin331@gmail.com>
2022-03-04 16:25:09 +0300

    Add error template

--- app.js
+++ app.js
@@ -73,8 +73,9 @@ app.all('*', (req, res, next) => {
 });
 
 app.use((err, req, res, next) => {
-  const { statusCode = 500, message = 'Something went wrong' } = err;
-  res.status(statusCode).send(message);
+  const { statusCode = 500, stack } = err;
+  const errMessage = (!err.message) ? 'Error' : err.message;
+  res.status(statusCode).render('error', { errMessage, stack });
 });
 
 app.listen(3000, () => {

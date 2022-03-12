14263060a66ef84d744fb1b469d5cedf9484165c
Nikita Lezhnev <catassasin331@gmail.com>
2022-03-12 13:18:15 +0300

    Add router logic

--- routes/users.js
+++ routes/users.js
@@ -1,15 +1,26 @@
 const express = require('express');
 
-const routes = express.Router();
 const User = require('../models/user');
 const router = require('./reviews');
+const wrapAsync = require('../utils/wrapAsync');
+
+const routes = express.Router();
 
 router.get('/register', (req, res) => {
   res.render('users/register');
 });
 
-router.post('/register', async (req, res) => {
-  res.send(req.body);
-});
+router.post('/register', wrapAsync(async (req, res) => {
+  try {
+    const { email, username, password } = req.body;
+    const user = new User({ email, username });
+    const registeredUser = await User.register(user, password);
+    req.flash('success', 'Welcome to YelpCamp!');
+    res.redirect('campgrounds');
+  } catch (e) {
+    req.flash('error', e.message);
+    res.redirect('/register');
+  }
+}));
 
 module.exports = router;

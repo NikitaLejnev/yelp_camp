9e8d05a6e5bd4b6c72a2276db06512d39054dae8
Nikita Lezhnev <catassasin331@gmail.com>
2022-03-16 10:17:35 +0300

    Style login form

--- public/js/validateForms.js
+++ public/js/validateForms.js
@@ -1,7 +1,7 @@
 // Example starter JavaScript for disabling form submissions if there are invalid fields
 (() => {
   // Fetch all the forms we want to apply custom Bootstrap validation styles to
-  const forms = document.querySelectorAll('.form-validated');
+  const forms = document.querySelectorAll('.validated-form');
 
   // Loop over them and prevent submission
   Array.from(forms)

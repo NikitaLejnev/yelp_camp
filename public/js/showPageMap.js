3550c8fb6db7f628b33c22393adebdde0627050b
Nikita Lezhnev <catassasin331@gmail.com>
2022-03-16 07:22:21 +0300

    Map is centered on the campground location

--- public/js/showPageMap.js
+++ public/js/showPageMap.js
@@ -1,9 +1,12 @@
-/* eslint-disable no-unused-vars */
 /* eslint-disable no-undef */
 mapboxgl.accessToken = mapBoxToken;
 const map = new mapboxgl.Map({
   container: 'map',
   style: 'mapbox://styles/mapbox/streets-v11',
-  center: [-74.5, 40],
-  zoom: 4,
+  center: coordinates,
+  zoom: 10,
 });
+
+new mapboxgl.Marker()
+  .setLngLat(coordinates)
+  .addTo(map);

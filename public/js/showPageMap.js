f2af21278236ed66152601400b43849b33de39e1
Nikita Lezhnev <catassasin331@gmail.com>
2022-03-16 07:49:10 +0300

    Customize map popup

--- public/js/showPageMap.js
+++ public/js/showPageMap.js
@@ -2,11 +2,20 @@
 mapboxgl.accessToken = mapBoxToken;
 const map = new mapboxgl.Map({
   container: 'map',
-  style: 'mapbox://styles/mapbox/streets-v11',
-  center: coordinates,
+  style: 'mapbox://styles/mapbox/light-v10',
+  center: campground.geometry.coordinates,
   zoom: 10,
 });
 
 new mapboxgl.Marker()
-  .setLngLat(coordinates)
+  .setLngLat(campground.geometry.coordinates)
+  .setPopup(
+    new mapboxgl.Popup({
+      offset: 25,
+    })
+      .setHTML(
+        `<h3>${campground.title}</h3>
+        <p>${campground.location}</p>`,
+      ),
+  )
   .addTo(map);
